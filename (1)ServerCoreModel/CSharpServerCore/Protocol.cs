6817bdafe31d7d8507b8aadedd1918dbe0d7eb80
HeeTaeKim996 <ttiiger1@naver.com>
2025-05-30 16:59:50 +0900

    Finished CSharpClientCore

---
+++ (1)ServerCoreModel/CSharpServerCore/Protocol.cs
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+using System;
+
+public enum C : UInt16
+{
+    C_TEST,
+
+    C_ENTER_GAME,
+
+    C_MAKE_ROOM,
+
+    C_JOIN_ROOM,
+
+    C_BACK_TO_LOBBY,
+
+    C_TEST_CHAT,
+
+    C_KEEP_ALIVE
+};
+
+public enum S : UInt16
+{
+    S_TEST,
+
+    S_ENTER_GAME,
+
+    S_INFORM_LOBBY_INFOS,
+
+    S_INFORM_ROOM_INFOS,
+
+    S_TEST_CHAT,
+};
+
---
+++ (1)ServerCoreModel/ServerCore/Protocol.h
@@ -0,0 +1,31 @@
+#pragma once
+
+enum : uint16
+{
+	C_TEST,
+
+	C_ENTER_GAME,
+
+	C_MAKE_ROOM,
+
+	C_JOIN_ROOM,
+
+	C_BACK_TO_LOBBY,
+
+	C_TEST_CHAT,
+
+	C_KEEP_ALIVE
+};
+
+enum : uint16
+{
+	S_TEST,
+
+	S_ENTER_GAME,
+	
+	S_INFORM_LOBBY_INFOS,
+
+	S_INFORM_ROOM_INFOS,
+
+	S_TEST_CHAT,
+};
